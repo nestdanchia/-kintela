19710c4aa41ad23af56511573e04c62a7b067b00
nestdanchia <ndchiariello@gmail.com>
2022-02-11 13:25:11 -0300

    mostrar fechas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
+import { Component, Inject, LOCALE_ID, OnInit, ViewChild } from '@angular/core';
 import * as moment from 'moment';
 import { formatDate } from "@angular/common";
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
-
+import { MatTable } from '@angular/material/table';
+// time zone https://www.section.io/engineering-education/nodejs-date-and-time-objects-with-moment/
 export interface EMPLOYEE  {
 
   name: string;
@@ -12,13 +13,15 @@ export interface EMPLOYEE  {
   inicio: Date;
 }
 
-const juanFecha=new Date("2016-09-01T10:10:12");
+
+const juanFecha=new Date("2019-08-01T10:11:12");
+//const juanFecha=moment('2014-02-27T10:00:00','D/M/YYYY').toDate()
 console.log('Juan fecha',juanFecha)
 // ISO 8601
-const pedroFecha=new Date("2018-08-01T10:11:12");
+const pedroFecha=new Date("2020-06-01T10:11:12");
 // RFC 2822 invalid
-// RFC 3339:
-const monicaFecha=new Date("2016-09-01T10:11:12.123456-0500")
+// RFC 3339: año mes dia 
+const monicaFecha=new Date("2016-08-01T10:11:12.123456-0500")
 
   
 
@@ -29,6 +32,7 @@ const monicaFecha=new Date("2016-09-01T10:11:12.123456-0500")
 })
 export class AppComponent implements OnInit {
 
+  @ViewChild(MatTable,{ static: true }) tablaEmploye!: MatTable<EMPLOYEE[]>;
   modiSource:EMPLOYEE[]
  EMPLOYEE_DATA :EMPLOYEE[]=[
     {
@@ -50,13 +54,21 @@ export class AppComponent implements OnInit {
       inicio:monicaFecha
     },
   ]
-    
+  MonicaFecha !:Date
   displayedColumns: string[] = ['name', 'sueldo', 'cargo','inicio'];
  dataSource :EMPLOYEE[]
   
-  constructor(@Inject(LOCALE_ID) private locale: string, private snackbar: MatSnackBar) { }
+  constructor(@Inject(LOCALE_ID) private locale: string, private snackbar: MatSnackBar) { 
+    this.MonicaFecha=monicaFecha
+  }
   ngOnInit(): void {
-
+let dateString = 'Thu Jul 15 2016 19:31:44 GMT+0200 (CEST)';
+let dateObj = new Date(dateString);
+let momentObj = moment(dateObj).toDate()
+//var momentString = momentObj.format('YYYY-MM-DD'); // 2016-07-15
+let juanFecha=momentObj
+console.log('juan Fecha',juanFecha,juanFecha)
+    
 this.FirstSource()
     
   }
@@ -68,25 +80,30 @@ FirstSource()
  
   } else{
     this.dataSource =this.modiSource
+ 
   }
 
 }
 changeSource(newSource){
    this.dataSource = newSource ;
+
 }
 updateFechaAval(aval: any, fechaAval:Date) {
 console.log(moment(fechaAval).isValid());
 console.log('aval:',aval.name)
-   console.log('Locale',moment.locale())
+   console.log('Locale',moment.locale());
+   console.log('Fecha Ingreso',fechaAval)
+
    if(moment(fechaAval).isValid()){
-  // let fecha = formatDate(fechaAval, 'dd-MM-yyyy', this.locale);
-   // fecha es de tipo string
+   
+   let fecha = moment(fechaAval,'D/M/YYYY').toDate()
+   
     // console.log('aval con los cambios', fecha);
       this.modiSource=this.dataSource.map(obj=>obj.name==aval.name  && fechaAval !==undefined?{
-       ...obj,inicio:fechaAval
+       ...obj,inicio:fecha
       }:obj);
       this.changeSource(this.modiSource)
-     console.log(this.modiSource)
+ 
      
    }
    else{
--- src/app/moment.pipe.ts
+++ src/app/moment.pipe.ts
@@ -7,7 +7,8 @@ import * as moment from 'moment';
 export class MomentPipe implements PipeTransform {
 
   transform(value: Date | moment.Moment, dateFormat: string): any {
-    return moment(value).format(dateFormat);
+   
+    return moment(value).format(dateFormat)
 }
   }
 
